cd2382ee122047926860af7033851d5f971464b8
r1cards <Bazinga1233>
2020-04-17 13:17:00 +0100

    )

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,34 +8,40 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
+// This is the Main class of the game
 public class Main extends Application {
     View view;
     Model model;
     Controller controller;
+//  This is the constructor for the Main Class
     public static void main(String[] args) {
         launch(args);
     }
+//  Tis method starts the start stage of the game which then if followed by the main stage
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("MVC App");
+//      Here we set the name of the stage and its properties
+        stage.setTitle("Counting game");
         stage.setResizable(false);
         stage.setAlwaysOnTop(true);
+//      Here we setup the start button
         Button startButton = new Button("Start");
-//        Start Button
         startButton.setLayoutX(600);
         startButton.setLayoutY(400);
         startButton.setScaleX(4.5);
         startButton.setScaleY(3);
         startButton.setFont(Font.font("Verdana", FontWeight.BOLD, 17));
         startButton.setStyle("-fx-background-color: orange; -fx-text-fill: white");
+//      Here we setup the stage for the start screen
         Pane start = new Pane();
+//      Here we set the background of the start screen
         start.setStyle("-fx-focus-color: transparent; -fx-faint-focus-color: transparent; -fx-background-image: url(/images/backgroundStart.png); -fx-background-repeat: none");
         start.getChildren().addAll(startButton);
+//      If the start button is pressed we start the main stage of the game
         startButton.setOnMouseClicked(event -> {
+//          Setting up the main stage
             Pane root = new Pane();
             stage.setScene(new Scene(root, 1280, 720));
             stage.show();
@@ -45,8 +51,10 @@ public class Main extends Application {
             model.setEquation(screenObject);
             controller = new Controller(model, view);
         });
+//      Here we set the stage for the start screen
         stage.setScene(new Scene(start, 1280, 720));
         stage.show();
+//      Here we make sure the game is turned off when the window is closed
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent event) {
